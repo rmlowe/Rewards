354b815e978e76e1cd1512c8aef21007b14d6827
Robert Lowe <robertmlowe@rmlowe.com>
2018-07-28 01:18:43 +0800

    0403 Creating A View

--- grails-app/controllers/rewards/InventoryController.groovy
+++ grails-app/controllers/rewards/InventoryController.groovy
@@ -6,6 +6,12 @@ class InventoryController {
         render "Here is a list of everything."
     }
 
+    def edit() {
+        def productName = "Breakfast Blend"
+        def sku = "BB01"
+        [product:productName, sku:sku]
+    }
+
     def remove() {
         render "You have one less than before."
     }
