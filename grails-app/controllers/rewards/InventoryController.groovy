6097126f773d56dd6372fcea3fc785aabbe1ffbb
Robert Lowe <robertmlowe@rmlowe.com>
2018-07-28 00:58:42 +0800

    Initial commit

---
+++ grails-app/controllers/rewards/InventoryController.groovy
@@ -0,0 +1,12 @@
+package rewards
+
+class InventoryController {
+
+    def index() {
+        render "Here is a list of everything."
+    }
+
+    def remove() {
+        render "You have one less than before."
+    }
+}
