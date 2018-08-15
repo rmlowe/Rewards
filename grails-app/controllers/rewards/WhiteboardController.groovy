a41e8e4ab6bd8c9c869e2291db4325ca603d051b
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-16 06:59:34 +0800

    0701 Variables

---
+++ grails-app/controllers/rewards/WhiteboardController.groovy
@@ -0,0 +1,27 @@
+package rewards
+
+class WhiteboardController {
+
+    def index() { }
+
+    def variables() {
+        def myTotal = 1
+        render("Total: " + myTotal)
+        render("</br>" + myTotal.class)
+        myTotal = myTotal + 1
+        render("</br>New Total: " + myTotal + "</br>")
+
+        def firstName = "Mike"
+        render("</br>Name: " + firstName)
+        render("</br>" + firstName.class)
+        firstName = firstName + 1
+        render("</br>New Name: " + firstName + "</br>")
+
+        def today = new Date("2/1/2014")
+        render("</br>Today is: " + today)
+        render("</br>" + today.class)
+        today = today + 1
+        render("</br>New Date: " + today + "</br>")
+    }
+
+}
