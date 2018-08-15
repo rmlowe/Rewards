c452a200908fc08c0d1ee384daa33193a56ae6ad
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-16 02:20:53 +0800

    0509 Constraints - Field Value Size

--- grails-app/domain/rewards/Customer.groovy
+++ grails-app/domain/rewards/Customer.groovy
@@ -12,7 +12,7 @@ class Customer {
         phone()
         firstName(nullable: true)
         lastName(nullable: true)
-        email(nullable: true)
-        totalPoints(nullable: true)
+        email(nullable: true, maxSize: 255)
+        totalPoints(nullable: true, max: 10)
     }
 }
