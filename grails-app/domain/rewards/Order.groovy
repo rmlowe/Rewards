c7073a5acaf86c43fd23c37eea7cf3a28679f20a
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-16 00:18:45 +0800

    0503 Relationships Between Domain Classes

--- grails-app/domain/rewards/Order.groovy
+++ grails-app/domain/rewards/Order.groovy
@@ -4,6 +4,8 @@ class Order {
     Date orderDate
     Integer orderNumber
     Float orderTotal
+    static belongsTo = [customer:Customer]
+    static hasMany = [orderItems:OrderItem]
 
     static constraints = {
     }
