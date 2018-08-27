78b2e8f4f78bf5bebba24030d3349456f5229292
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-28 01:41:12 +0800

    0801 List Queries

--- grails-app/controllers/rewards/CustomerController.groovy
+++ grails-app/controllers/rewards/CustomerController.groovy
@@ -3,6 +3,11 @@ package rewards
 class CustomerController {
     static scaffold = Customer
 
+    def lookup() {
+        def customerInstance = Customer.list(sort: "lastName", order: "desc", max: 5, offset: 5)
+        [customerInstanceList: customerInstance]
+    }
+
     //def index() { }
 
     def checkin() {}
