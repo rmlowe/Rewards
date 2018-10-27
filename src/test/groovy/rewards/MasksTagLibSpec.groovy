e6dc34ff76bb817bc8a877a72c875e5b8b14769d
Robert Lowe <robertmlowe@rmlowe.com>
2018-10-27 20:54:40 +0800

    0909 Formatting The Phone Number

---
+++ src/test/groovy/rewards/MasksTagLibSpec.groovy
@@ -0,0 +1,18 @@
+package rewards
+
+import grails.testing.web.taglib.TagLibUnitTest
+import spock.lang.Specification
+
+class MasksTagLibSpec extends Specification implements TagLibUnitTest<MasksTagLib> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
